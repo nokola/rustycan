fb1c5496198eb2733b72fb2a39a734542e67e49a
Nokola <nikolami@outlook.com>
2022-12-26 22:09:41 -0800

    Add tests: empty and basic case

---
+++ rustycan_macro/tests/tests.rs
@@ -0,0 +1,23 @@
+use rustycan_macro::rustycan_ui;
+
+#[test]
+fn compile_empty() {
+    rustycan_ui! {
+    }
+}
+
+#[test]
+fn compile_basic() {
+    rustycan_ui! {
+        VertStack (
+            Label "Hello world" (before=1x after=1x) // center
+            HorizStack (
+                before_first=1x // right align
+                between=10
+
+                Button "Ok"
+                Button "Cancel"
+            )
+        )
+    }
+}
